19baff4fa7e4bbf11be8ca6756b889e423fb6abf
midorigoke <midorigoke@desumachi.tk>
2017-02-02 23:38:41 +0900

    プロジェクト ファイルを追加します。

---
+++ bouyomi_relay_cui/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bouyomi_relay_cui
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
